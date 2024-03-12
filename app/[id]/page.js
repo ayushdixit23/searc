4fd41b53a2d141f68d90aabba1b0ec303f4684a9
Ayush Dixit <117524157+ayushdixit23@users.noreply.github.com>
2024-03-12 16:41:44 +0530

    12

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -13,21 +13,23 @@ import Community from "../component/Community";
 import Store from "../component/Store";
 function page({ params }) {
   const [coms, setComs] = useState([]);
-  const [bio, setBio] = useState();
+  const [bio, setBio] = useState("");
   const [product, setProduct] = useState([]);
-  const [user, setUser] = useState(true)
+  const [user, setUser] = useState(true);
 
   const fetchData = async () => {
     try {
       const res = await axios.get(
-        `https://back.grovyo.xyz/api/getprositedetails/${params.id}`
+        ` https://back.grovyo.xyz/api/getprositedetails/${params.id}`
       );
+      console.log(res.data);
+      setBio(res.data.data.userDetails);
+      setComs(res.data.data.communitywithDps);
+      setProduct(res.data.data.productsWithDps);
+      console.log(bio);
       if (res.data.success) {
-        setBio(res.data.data.userDetails);
-        setComs(res.data.data.communitywithDps);
-        setProduct(res.data.data.productsWithDps);
       } else {
-        setUser(false)
+        setUser(false);
       }
     } catch (error) {
       console.log(error);
@@ -41,7 +43,7 @@ function page({ params }) {
     return (
       <>
         <section className="relative z-10 flex select-none flex-col justify-center w-full  items-center bg-black h-screen py-[120px]">
-          <div className="container w-full" >
+          <div className="container w-full">
             <div className="w-full flex">
               <div className="w-full px-4">
                 <div className="w-full text-center">
@@ -66,18 +68,20 @@ function page({ params }) {
           </div>
         </section>
       </>
-    )
+    );
   }
 
   return (
     <div className="h-screen w-screen bg-gradient-to-r from-[#000000] to-[#2f2f2f] flex flex-col items-center justify-center">
       {/* header */}
-      <div className=" justify-center items-center w-[100%] flex ">
+      {/* <div className=" justify-center items-center w-[100%] flex ">
         <div className="  w-full flex justify-between items-center px-2 ">
-          <div className={`h-[55px] bg-black top-3 absolute flex text-[#fff] items-center justify-center rounded-3xl ring-2 ring-white`}>
+          <div
+            className={h-[55px] bg-black top-3 absolute flex text-[#fff] items-center justify-center rounded-3xl ring-2 ring-white}
+          >
             <div className="h-[55px] w-[55px] ring-2 ring-[#fff] rounded-3xl">
               <img
-                src={`${bio?.dp}`}
+                src={${bio?.dp}}
                 alt="dp"
                 className="rounded-3xl h-[55px] w-[55px] "
               />
@@ -87,34 +91,29 @@ function page({ params }) {
               <div className=" text-[12px]">@{bio?.username}</div>
             </div>
           </div>
-          {/* <div>
-          <div className="bg-white px-4 py-2 font-semibold rounded-full">
-            Download 
-          </div>
-        </div> */}
+        
         </div>
-      </div>
+      </div> */}
       {/* main */}
 
-      <div className="h-[95%] bg-red-400 w-[98%] rounded-2xl no-scrollbar overflow-auto">
+      <div className="h-[100%] w-[100%] no-scrollbar overflow-auto">
         <div className="bg-white text-black overflow-auto no-scrollbar w-full h-full">
           {/* Header */}
           <div>
             {bio?.temp?.length > 0 ? (
-              <div
-                data-aos="fade-down-right"
-                className="w-full bg-green-700"
-                dangerouslySetInnerHTML={{ __html: bio?.temp }}
-              ></div>
+              <>
+                <div
+                  className="w-full h-[80vh] pn:max-sm:hidden bg-green-700"
+                  dangerouslySetInnerHTML={{ __html: bio?.temp }}
+                ></div>
+                <div
+                  className="w-full h-[80vh] sm:hidden bg-green-700"
+                  dangerouslySetInnerHTML={{ __html: bio?.temp1 }}
+                ></div>
+              </>
             ) : (
-              <div
-                data-aos-delay="50"
-                data-aos-duration="1500"
-                data-aos-easing="ease-in-out"
-                data-aos="fade-up"
-                className="flex pn:max-md:flex-col-reverse items-center py-4 w-[100%] h-[60%]"
-              >
-                <div className="flex  flex-col md:w-[50%] h-[100%] justify-center items-center">
+              <div className="flex pn:max-md:flex-col-reverse items-center py-4 w-[100%] h-[60%]">
+                {/* <div className="flex  flex-col md:w-[50%] h-[100%] justify-center items-center">
                   <div className="flex flex-col w-[60%] h-[60%] justify-center items-center">
                     <div className="md:text-[25px] text-center text-black font-bold my-2">
                       "Unleash your passion to personalize your space and show
@@ -135,16 +134,46 @@ function page({ params }) {
                     src={Logo}
                     className="h-[60%] w-[60%] object-contain"
                   />
+                </div> */}
+                <div className="flex bg-white flex-col items-center w-[100%] h-[80vh]">
+                  <div className="h-[100%] w-[100%] flex justify-center flex-col overflow-hidden items-center ">
+                    <div className="h-[100%] w-[100%] bg-bggg bg-cover mt-[0%] bg-center"></div>
+                    <div className="h-[100px] w-[100px] flex flex-col justify-center items-center -mt-6">
+                      {/* <img
+                        src={${bio?.dp}}
+                        alt="dp"
+                        className="rounded-3xl h-[55px] ring-2 ring-white w-[55px] "
+                      /> */}
+                      <div className="h-[100px] flex justify-center items-center flex-col w-[150px]">
+                        <div className="text-[18px] font-bold">
+                          {bio?.fullname}
+                        </div>
+                        <div className=" text-[12px] font-medium">
+                          @{bio?.username}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex flex-col md:w-[100%] h-[50%] justify-center items-center">
+                    <div className="flex flex-col w-[80%] h-[60%] justify-center items-center">
+                      <div className="md:text-[50.5px] text-center text-black font-bold my-2">
+                        "Unleash your passion to
+                        <a className="text-rose-400"> personalize </a> your
+                        space and show the world the extraordinary things you're
+                        capable of “
+                      </div>
+                      <div className="bg-[#0075FF] text-white font-sans mt-6 font-medium text-[16px] w-[160px] flex justify-center items-center  py-2 rounded-lg">
+                        Edit now
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             )}
 
             {/* About section */}
             <div
-              data-aos-delay="50"
-              data-aos-duration="1500"
-              data-aos-easing="ease-in-out"
-              data-aos="fade-up"
+             
             >
               <Bio bio={bio} />
             </div>
@@ -165,12 +194,7 @@ function page({ params }) {
 
             {/* Store section */}
             {product.length > 0 ? (
-              <div
-                data-aos-delay="50"
-                data-aos-duration="1500"
-                data-aos-easing="ease-in-out"
-                data-aos="fade-up"
-              >
+              <div>
                 <Store product={product} />
               </div>
             ) : (
@@ -195,17 +219,8 @@ function page({ params }) {
               </div>
             )}
 
-            <div
-              data-aos-delay="50"
-              data-aos-duration="1500"
-              data-aos-easing="ease-in-out"
-              data-aos="fade-up"
-              className="py-2 items-center justify-between px-2 w-[100%] mt-4 border-t-2 border-[#f9f9f9]  flex flex-row"
-            >
-              <div
-                data-aos="fade-down-right"
-                className="flex flex-row items-center"
-              >
+            <div className="py-2 items-center justify-between px-2 w-[100%] mt-4 border-t-2 border-[#f9f9f9]  flex flex-row">
+              <div className="flex flex-row items-center">
                 <Image src={Logo} className="h-[35px] w-[35px]" />
                 <div className="text-black text-[18px] font-bold font-sans">
                   Grovyo
--- app/prosite/[id]/page.jsx
+++ app/prosite/[id]/page.jsx
@@ -1,43 +1,146 @@
-
-"use client"
+"use client";
+import Image from "next/image";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
 
 function page({ params }) {
-	const [data, setData] = useState(null);
-	function decodeUsernameAndNumber(encodedString) {
-		const decodedString = decodeURIComponent(encodedString);
-		return decodedString;
-	}
-	const fetchData = async () => {
-		try {
+  const [bio, setBio] = useState("");
+  const [user, setUser] = useState(true);
 
-			const res = await axios.post(
-				"https://work.grovyo.xyz/api/v1/getprosite",
-				{
-					username: decodeUsernameAndNumber(params.id),
-				}
-			);
-			setData(res.data?.prosite);
-		} catch (error) {
-			console.log(error);
-		}
-	};
-	useEffect(() => {
-		if (params.id) {
-			fetchData();
-		}
-	}, [params.id]);
+  const fetchData = async () => {
+    try {
+      const res = await axios.get(
+        ` https://back.grovyo.xyz/api/getprositedetails/${params.id}`
+      );
+      console.log(res.data);
+      setBio(res.data.data.userDetails);
 
-	return (
-		<div>
+      console.log(bio);
+      if (res.data.success) {
+      } else {
+        setUser(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, [params.id]);
 
-			<div>
-				<div dangerouslySetInnerHTML={{ __html: data }} />
-			</div>
+  if (!user) {
+    return (
+      <>
+        <section className="relative z-10 flex select-none flex-col justify-center w-full  items-center bg-black h-screen py-[120px]">
+          <div className="container w-full">
+            <div className="w-full flex">
+              <div className="w-full px-4">
+                <div className="w-full text-center">
+                  <h2 className="mb-2 text-[50px] font-bold leading-none text-white sm:text-[80px] md:text-[100px]">
+                    404
+                  </h2>
+                  <h4 className="mb-3 text-[22px] font-semibold leading-tight text-white">
+                    Oops! That page can't be found
+                  </h4>
+                  <p className="mb-8 text-lg text-white">
+                    The page you are looking for it maybe deleted
+                  </p>
+                  <a
+                    href="/"
+                    className="inline-block rounded-lg border border-white px-8 py-3 text-center text-base font-semibold text-white transition hover:bg-blue-400 hover:text-primary"
+                  >
+                    Go To Home
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
+    );
+  }
 
-		</div>
-	);
+  return (
+    <div className="h-screen w-screen bg-gradient-to-r from-[#000000] to-[#2f2f2f] flex flex-col items-center justify-center">
+      <div className="h-[100%] w-[100%] no-scrollbar overflow-auto">
+        <div className="bg-white text-black overflow-auto no-scrollbar w-full h-full">
+          {/* Header */}
+          <div>
+            {bio?.temp?.length > 0 ? (
+              <>
+                <div
+                  className="w-full h-[80vh] pn:max-sm:hidden bg-green-700"
+                  dangerouslySetInnerHTML={{ __html: bio?.temp }}
+                ></div>
+                <div
+                  className="w-full h-[80vh] sm:hidden bg-green-700"
+                  dangerouslySetInnerHTML={{ __html: bio?.temp1 }}
+                ></div>
+              </>
+            ) : (
+              <div className="flex pn:max-md:flex-col-reverse items-center py-4 w-[100%] h-[60%]">
+                {/* <div className="flex  flex-col md:w-[50%] h-[100%] justify-center items-center">
+                  <div className="flex flex-col w-[60%] h-[60%] justify-center items-center">
+                    <div className="md:text-[25px] text-center text-black font-bold my-2">
+                      "Unleash your passion to personalize your space and show
+                      the world the extraordinary things you're capable of “
+                    </div>
+                    <div className="text-[16px] text-center text-black font-medium">
+                      Prosite : fully customizable layouts for an enhanced
+                      personalization experience
+                    </div>
+                    <div className="bg-[#0075FF] text-white font-sans mt-6 font-medium text-[16px] w-[160px] flex justify-center items-center  py-2 rounded-lg">
+                      Edit now
+                    </div>
+                  </div>
+                </div>
+                <div className="h-[60%] md:w-[50%] flex justify-center items-center ">
+                  {" "}
+                  <Image
+                    src={Logo}
+                    className="h-[60%] w-[60%] object-contain"
+                  />
+                </div> */}
+                <div className="flex bg-white flex-col items-center w-[100%] h-[80vh]">
+                  <div className="h-[100%] w-[100%] flex justify-center flex-col overflow-hidden items-center ">
+                    <div className="h-[100%] w-[100%] bg-bggg bg-cover mt-[0%] bg-center"></div>
+                    <div className="h-[100px] w-[100px] flex flex-col justify-center items-center -mt-6">
+                      {/* <img
+                        src={${bio?.dp}}
+                        alt="dp"
+                        className="rounded-3xl h-[55px] ring-2 ring-white w-[55px] "
+                      /> */}
+                      <div className="h-[100px] flex justify-center items-center flex-col w-[150px]">
+                        <div className="text-[18px] font-bold">
+                          {bio?.fullname}
+                        </div>
+                        <div className=" text-[12px] font-medium">
+                          @{bio?.username}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex flex-col md:w-[100%] h-[50%] justify-center items-center">
+                    <div className="flex flex-col w-[80%] h-[60%] justify-center items-center">
+                      <div className="md:text-[50.5px] text-center text-black font-bold my-2">
+                        "Unleash your passion to
+                        <a className="text-rose-400"> personalize </a> your
+                        space and show the world the extraordinary things you're
+                        capable of “
+                      </div>
+                      <div className="bg-[#0075FF] text-white font-sans mt-6 font-medium text-[16px] w-[160px] flex justify-center items-center  py-2 rounded-lg">
+                        Edit now
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default page;
